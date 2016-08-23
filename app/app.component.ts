48778dca759a15754b71fb2e4c4b3e6b37f61436
Mandeep <mandeep419.singh@gmail.com>
2016-08-23 10:07:41 -0500

    header component added and html files separated
    header component added and html files separated

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  templateUrl: 'HtmlPartials/app.component.html'
 })
+
+
 export class AppComponent { }
